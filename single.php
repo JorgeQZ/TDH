2426dad3808d8dc9c3319133e11a308cf5d966c3
JorgeQZ <jorge_qzg@hotmail.com>
2021-07-26 17:17:16 -0500

    proy sus updates

--- single.php
+++ single.php
@@ -1,31 +1,14 @@
 <?php get_template_part('template/header'); ?>
 
 <div class="contenedor-page">
-
     <?php
-        $post_type = get_post_type();
-        if ($post_type === "proy_sustentables") {
-            echo get_field('banner-slider');
-            echo '<div class="container">';
-            if (have_posts()):
-                while (have_posts()):
-                    the_post();
-            the_content();
-            endwhile;
-            endif;
-            echo '</div>';
-        } else {
-            if (have_posts()):
-            while (have_posts()):
-                the_post();
-            the_content();
-            endwhile;
-            endif;
-        }
-
-
+        if (have_posts()):
+        while (have_posts()):
+            the_post();
+        the_content();
+        endwhile;
+        endif;
     ?>
-
 </div>
 
 <?php
